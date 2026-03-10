1a1b14f457b8b289be9ab02db23c686229a44877
Jeet Desai <jeetdesai32800@gmail.com>
2026-03-10 12:42:17 +0530

    Guess the number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello World!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret number is {}.", secret_number);
+
+    loop {
+
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        match guess.trim().cmp(&secret_number.to_string()) {
+            Ordering::Less => println!("Less"),
+            Ordering::Greater => println!("Greater"),
+            Ordering::Equal => {
+                println!("Equal, You win!");
+                break;
+            }
+        }
+    }
 }
+
